3523f1f3466049e6da3d3fd93b37926fbe364d7b
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-12-17 15:56:15 +0100

    Added js file
    Added the code to improve the look of html page with a section that scroll horizontally while the user scroll vertically and to make burger menu for mobile page

---
+++ docs/js/code.js
@@ -0,0 +1,56 @@
+const stickySection = document.querySelector('.sticky');
+const offsetTop = stickySection.parentElement.offsetTop;
+const scrollSections = stickySection.querySelectorAll('.scroll_section');
+
+window.addEventListener('scroll', (e) => {
+    transform(stickySection);
+})
+
+function transform() {
+    let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100 / 4;
+    percentage = percentage < 0 ? 0 : percentage > 500 ? 500 : percentage;
+    for(let i = 0; i < scrollSections.length; i++) {
+        scrollSections[i].style.transform = `translate3d(calc(100vw*${i} + ${-(percentage)}vw), 0, 0)`;
+    }
+}
+
+function returnToBegin() {
+    window.scroll("0px", "0px");
+}
+
+function toggleShowMenu(section) {
+    const x = document.querySelector('.burger-symbol');
+    const body = document.querySelector('body');
+    const overlay = document.querySelector('.overlay');
+    if (x.classList != "burger-symbol change" && section.classList != "logo-link") {
+        body.style = `overflow: hidden`;
+        overlay.style = `display: inline; position: fixed; background-color: black; width: 100%; height: 100%; padding: calc(1.5rem + 80px) 0 0 34px; z-index: 1;`;
+    } else {
+        body.style = `overflow: scroll`;
+        overlay.style = `display: none;`;
+    }
+
+    if (section.classList != "logo-link" || (section.classList == "logo-link" && x.classList == "burger-symbol change")) {
+        x.classList.toggle("change");
+    }
+}
+
+/* if window.maxWidth <= "479px" {
+    var d = document.querySelectorAll('.paragraph'), i, w, width, height;
+
+    for(i = 0; i < d.length; i++) {
+        width = d[i].offsetWidth;
+        height = d[i].offsetHeight;
+
+        for (w = width; w; w--) {
+            d[i].style.width = w + 'px';
+            if (d[i].offsetHeight !== height) break;
+        }
+    
+        if (w < d[i].scrollWidth) {
+            d[i].style.width = d[i].style.maxWidth = d[i].scrollWidth + 'px';
+        } else {
+            d[i].style.width = (w + 1) + 'px';
+        }
+    }
+} */
